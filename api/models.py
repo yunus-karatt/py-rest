4541f92a2c693781c7f41b4cf52f3439e6190e41
yunus <karattyunus@gmail.com>
2025-09-21 20:57:17 +0530

    serializers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
